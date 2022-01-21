f0ce2cc8fd9226064734a4c4e5085035dc528396
ransomedesign <info@ransomedesign.co.uk>
2022-01-21 11:15:17 +0000

    Completed module

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+"""
+Create login and logout functionality for users.
+"""
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index' ), name='logout'),
+]
